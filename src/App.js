659a74b04ed4d831361dab9d27dd38f87acea1c1
Priyam <priyamgupta340@gmail.com>
2020-11-23 02:23:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
-import logo from './logo.svg';
+
 //import './App.css';
-import { Link } from 'react-router-dom'
-import './scss/style.scss';
-import {
-  CButton,
-  CCard,
-  CCardBody,
-  CCardGroup,
-  CCol,
-  CContainer,
-  CForm,
-  CInput,
-  CInputGroup,
-  CInputGroupPrepend,
-  CInputGroupText,
-  CRow
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
+
 function App() {
   return (
     <div className="c-app c-default-layout  align-items-center">
