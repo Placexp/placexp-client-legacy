ad97c09fe933e217b6a2f80a07950777316a45bc
HimanshuBarak <66054954+HimanshuBarak@users.noreply.github.com>
2020-11-25 17:20:13 +0530

    intial commit

--- src/Component/Auth/Login.js
+++ src/Component/Auth/Login.js
@@ -1,79 +1,43 @@
-import logo from './logo.svg';
-//import './App.css';
-import { Link } from 'react-router-dom'
-import '../../scss/style.scss';
-import {
-  CButton,
-  CCard,
-  CCardBody,
-  CCardGroup,
-  CCol,
-  CContainer,
-  CForm,
-  CInput,
-  CInputGroup,
-  CInputGroupPrepend,
-  CInputGroupText,
-  CRow
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-const Login=()=> {
-  return (
-    <div className="c-app c-default-layout flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md="8">
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupPrepend>
-                        <CInputGroupText>
-                          <CIcon name="cil-user" />
-                        </CInputGroupText>
-                      </CInputGroupPrepend>
-                      <CInput type="text" placeholder="Username" autoComplete="username" />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupPrepend>
-                        <CInputGroupText>
-                          <CIcon name="cil-lock-locked" />
-                        </CInputGroupText>
-                      </CInputGroupPrepend>
-                      <CInput type="password" placeholder="Password" autoComplete="current-password" />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs="6">
-                        <CButton color="primary" className="px-4">Login</CButton>
-                      </CCol>
-                      <CCol xs="6" className="text-right">
-                        <CButton color="link" className="px-0">Forgot password?</CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                      labore et dolore magna aliqua.</p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>Register Now!</CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
-  );
+import React,{useState} from 'react'
+import './login.css'
+import usericon from './login.gif'
+
+function Login({setauthpage}) {
+    //the user object 
+    const [user, setuser] = useState({
+        email:'',
+        password:''
+    })
+    console.log(user)
+    let handlesubmit=e=>{
+        e.preventDefault();
+        
+        //verifying the creditantials
+    }
+    return (
+        <div className="container">
+          
+          <div className="left_container">
+             <h1>Welcome Back!</h1>
+              <h2>Are you new around here? Sign up to get connected with us.</h2>
+              <button onClick={()=>setauthpage("signup")}>SIGN UP</button>
+        </div>
+        <div className="login_container">
+            <h1>LOGIN TO PLACEXP</h1>
+            <img src={usericon} alt="user icon" className="user_icon"/>
+            <form className="login_form">
+                <h5>Email</h5>
+               <input type="email" value={user.email} onChange={e=>setuser({...user,email:e.target.value})} placeholder="Enter your email"/>
+                <h5>Password</h5>
+               <input type="password" value={user.password} onChange={e=>setuser({...user,password:e.target.value})}  placeholder="Enter your password"  value={user.password}/>
+                <h3 className="forgotpass" onClick={()=>setauthpage("forgot") } style={{color:"#00bcd4"}}>Forgot Password?</h3>
+                <h3>Not Registered yet?<a onClick={()=>setauthpage("signup")} style={{color:"#00bcd4"}}>Sign up</a></h3>
+               <button>LOGIN</button>
+            </form>
+        </div>
+        </div>
+        
+    )
 }
 
 export default Login;
