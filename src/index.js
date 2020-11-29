be56487b0ab889155fff8c0e93e1e467036210db
Priyam <priyamgupta340@gmail.com>
2020-11-29 20:20:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React ,{Suspense}from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-
+import * as serviceWorker from "./serviceWorker";
 import reportWebVitals from './reportWebVitals';
 
 import {
@@ -43,3 +43,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
