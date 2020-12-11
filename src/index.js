49bc281359e0f741f942f3cc757b99d3bab66269
GowthamNats <72858459+GowthamNats@users.noreply.github.com>
2020-12-11 07:48:05 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"
+import Events from "./events"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Events />, document.getElementById("EvEnTs"));
