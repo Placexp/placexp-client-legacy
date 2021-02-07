850d37b7f177c889953def5ba48f19c637d85938
Jennifer <clemensj3@users.noreply.github.com>
2021-02-07 00:29:23 -0800

    Update Header.js

--- src/Component/Layout/Header.js
+++ src/Component/Layout/Header.js
@@ -1,9 +1,9 @@
-import React,{useContext} from "react";
-import Context from '../../context';
+import React, { useContext } from "react";
+import Context from "../../context";
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 const Header = () => {
-    const { State, dispatch } = useContext(Context);
+  const { State, dispatch } = useContext(Context);
   return (
     <header id="head">
       <Navbar variant="light" expand="lg" collapseOnSelect>
@@ -14,9 +14,24 @@ const Header = () => {
             <Nav className="ml-auto">
               <Nav.Link href="about">About Us</Nav.Link>
               <Nav.Link href="service">Services</Nav.Link>
-              <Nav.Link > <Link className="link" to="events">Events</Link></Nav.Link>
-              <Nav.Link > {State.isAuth?(<Link className="link" to="logout">Logout</Link>):(<Link className="link" to="auth">Sign In</Link>)}</Nav.Link>
-              
+              <Nav.Link>
+                {" "}
+                <Link className="link" to="events">
+                  Events
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                {" "}
+                {State.isAuth ? (
+                  <Link className="link" to="logout">
+                    Logout
+                  </Link>
+                ) : (
+                  <Link className="link" to="auth">
+                    Sign In
+                  </Link>
+                )}
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
