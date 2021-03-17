065800cb3dc8cbd4bb65b90aa9a95dc6eb50ed98
akhilr2505 <akhil.rudrawar@gmail.com>
2021-03-17 21:03:43 +0530

    added interview page changes

--- src/Pages/Interview.js
+++ src/Pages/Interview.js
@@ -1,86 +1,100 @@
-import React, {useRef, useEffect, useState,Fragment } from "react";
-import SunEditor from 'suneditor-react';
-import DateTimePicker from 'react-datetime-picker';
-import 'suneditor/dist/css/suneditor.min.css';
-import axios from 'axios';
-import FuzzySearch from 'fuzzy-search';
-import Swal from 'sweetalert2';
-import {Url} from '../Url';
-import './Interview.css'
+import React, { useEffect, useState, useContext } from "react";
+
+import SunEditor from "suneditor-react";
+import DateTimePicker from "react-datetime-picker";
+import "suneditor/dist/css/suneditor.min.css";
+import axios from "axios";
+import FuzzySearch from "fuzzy-search";
+import interviewsData from "./interviewsData.json";
+import Fuse from "fuse.js";
+import Swal from "sweetalert2";
+import { Url } from "../Url";
+import "./Interview.css";
 import { useHistory } from "react-router-dom";
-import Header from '../Component/Layout/Header';
-import { useCookies } from 'react-cookie';
-import "./Button.css"
-const Interview=()=>{
-  //window.location('/interview')
-  const history = useHistory();
-  const [interview ,setInterview]=useState([]);
-  const fuzzySearcher = new FuzzySearch(interview, ['title','company']);
-  const [search,setSearch]=useState('');
-  const result = fuzzySearcher.search(search);
- 
-  const [isLoading,setLoading]=useState(true);
-  useEffect(async() => {
-        
-    const response= await axios({
-     method: 'post',
-     withCredentials: true,
-     url: Url()+"/post/getall",
-   });
-   console.log(response);
-   setInterview(response.data.data);
-   setLoading(false)
- }, [isLoading]);
+import Header from "../Component/Layout/Header";
+import { useCookies } from "react-cookie";
+import "./Button.css";
 
- const goToEvent = i => {
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
- history.push("/interview/" + i);
-};
-return (
-<div >
-<Header/>
+const Interview = () => {
+  const [query, updateQuery] = useState("");
+  const [data, setData] = useState("");
 
-<br/>
-<div className="container">
+  const fuse = new Fuse(interviewsData, {
+    keys: ["name", "branch", "company", "difficulty"],
+    includeScore: true,
+  });
 
-<div className="events__container">
-          
-          <h4>Interviews</h4>
-          <center><input placeholder="Search" value={search} onChange={e=>setSearch(e.target.value)}/></center> 
-          
-          <br/>
-          <div className="card__section">
-        
-     
-{result.length>0?result.map((item)=>(
- <div class="events__card h-100" onClick={e=>goToEvent(item._id)}>
- <div class="card-body text-center">
-   
-   <span className="text-primary">
-     <h3 style={{
-       fontSize: '20px',
-       fontWeight: '700',
+  const results = fuse.search(query);
+  const interviewResults = query
+    ? results.map((interview) => interview.item)
+    : interviewsData;
 
-     }}>{item.company}</h3>
-   </span>
-   <span className="secondary" style={{
-       fontSize: '15px',
-       //fontWeight: '700',
+  function onSearch({ currentTarget }) {
+    updateQuery(currentTarget.value);
+  }
 
-     }}>
-     {item.postTitle}
-   </span>
-   <br />
-  
- </div>
-</div>
-)):"Try Again"}
-</div>
-</div>
-</div>
-     
-</div>
-);
+  const handleSelect = (e) => {
+    updateQuery(e);
+  };
 
+  return (
+    <>
+      <div className="home"></div>
+      <Header />
+      <div className="Interview__page">
+        <div className="Interview__container">
+          <div className="Int__heading">
+            <h1>Interviews</h1>
+          </div>
+
+          <div className="search_filter">
+            <form className="search">
+              <label>Search</label>
+              <input type="text" value={query} onChange={onSearch} />
+            </form>
+
+            <div className="filterDiv">
+              <DropdownButton
+                id="dropdown-item-button"
+                title="Filter"
+                onSelect={handleSelect}
+                value={query}
+              >
+                <Dropdown.Item value="Amazon" as="button">
+                  Amazon
+                </Dropdown.Item>
+                <Dropdown.Item as="button">Wipro</Dropdown.Item>
+                <Dropdown.Item as="button">Paypal</Dropdown.Item>
+                <Dropdown.Item as="button">Adobe</Dropdown.Item>
+              </DropdownButton>
+            </div>
+          </div>
+
+          <div className="Int__section">
+            <div className="interviews">
+              {interviewResults.map((interview) => {
+                const { name, branch, company, difficulty } = interview;
+                return (
+                  <div className="Int__card">
+                    <h2 className="Int__name">{name}</h2>
+                    <div className="tag__area">
+                      <h3 className="Int__tag tag__header">tags: </h3>
+                      <div className="Int__tag">
+                        <div className="tag">{branch}</div>
+                        <div className="tag">{company}</div>
+                        <div className="tag">{difficulty}</div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Interview;
