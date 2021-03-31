55489122960deb480542131c09f9e655b3bb0c6c
priyam340 <44100074+priyam340@users.noreply.github.com>
2021-03-31 20:57:07 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -98,11 +98,9 @@ const Home = () => {
             <div class="row align-items-center">
               <div class="col-md-6">
                 <div class="jumbotron">
-                  <h1 class="heading">State-of-the-Art Design</h1>
+                  <h1 class="heading"> Interview Experience</h1>
                   <p class="lead">
-                    User-friendly and organised to the core while not
-                    compromising on the classiness. Let's just say it's modern
-                    and elegant, like you.
+                   A complete collection of interviews from formerly placed students to ease your placement journey 
                   </p>
                 </div>
               </div>{" "}
@@ -113,7 +111,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-
+ <h1 class="headingf">Comin Soon</h1>
           <div class="feature3">
             <div class="row align-items-center">
               <div class="col-md-5">
@@ -123,11 +121,10 @@ const Home = () => {
               <br className="d-md-none" />
               <div class="col-md-7">
                 <div class="jumbotron">
-                  <h1 class="heading">Socially Connected</h1>
+                  <h1 class="heading">DSPACE</h1>
                   <p class="lead">
-                    Lets you know the opinions and comments your colleagues have
-                    on an event or company and allows you to freely discuss your
-                    opinion.
+                   Past years question papers , reference materials and what not!!!
+Anything and Everything of your courses !!
                   </p>
                 </div>
               </div>
