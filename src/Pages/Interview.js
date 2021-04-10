2ccc13c7027c9aafe1cced9c14d12e0cfb49f518
Priyam <studentvit0@gmail.com>
2021-04-10 16:36:01 +0530

    Changes

--- src/Pages/Interview.js
+++ src/Pages/Interview.js
@@ -55,7 +55,13 @@ const Interview = () => {
     return ans;
   };
   const history = useHistory();
-  
+  const AllTags=value=>{
+    var tag=[];
+    var t=value.split(",");
+    for(let i=0;i<t.length;i++)
+    tag.push(<div className="tag">{t[i]}</div>)
+    return tag
+  }
   const goToEvent = i => {
 
     history.push("/interview/" + i);
@@ -107,7 +113,10 @@ Loading
                       <div className="Int__tag">
                         
                         <div className="tag">{item.company}</div>
-                        
+                        {
+
+                       AllTags(item.tags)
+                        }
                       </div>
                     </div>
                   </div>
