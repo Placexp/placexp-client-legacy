81ceaa1eb41bcfc3935e9556cb368ef1256a7149
GowthamNats <72858459+GowthamNats@users.noreply.github.com>
2021-09-20 19:34:19 +0530

    Update Header.js

--- src/Component/Layout/Header.js
+++ src/Component/Layout/Header.js
@@ -87,23 +87,23 @@ const Header = () => {
               )}
             </div>
           </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              data-toggle="dropdown"
-              role="button"
-              aria-haspopup="true"
-              aria-expanded="false"
-              href="/#"
-            >
-              Mock Interviews
-            </a>
-            <div class="dropdown-menu">
-              <Link className=" dropdown-item" to="/mockinterview">
-                View Mock Interviews
-              </Link>
-            </div>
-          </li>
+//           <li class="nav-item dropdown">
+//             <a
+//               class="nav-link dropdown-toggle"
+//               data-toggle="dropdown"
+//               role="button"
+//               aria-haspopup="true"
+//               aria-expanded="false"
+//               href="/#"
+//             >
+//               Mock Interviews
+//             </a>
+//             <div class="dropdown-menu">
+//               <Link className=" dropdown-item" to="/mockinterview">
+//                 View Mock Interviews
+//               </Link>
+//             </div>
+//           </li>
           <li class="nav-item">
             <a class="nav-link" href="/Contact">
               Contact Us
