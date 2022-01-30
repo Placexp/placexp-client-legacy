fd83d7da2bc69f9b164441a7f78eae7988ebb911
Sarath Adhithya <91727830+SarathAdhi@users.noreply.github.com>
2022-01-30 13:50:46 +0530

    Add files via upload

--- src/Pages/ClubEvent.js
+++ src/Pages/ClubEvent.js
@@ -1,59 +1,82 @@
-import React, { useEffect, useState, useContext } from "react";
+import React from 'react';
+import './ClubEvent.css'
 
-import "./ClubEvent.css";
+function ClubEvent() {
 
-const ClubEvent = () => {
+    function showNavbar(){
+        document.querySelector(".header-nav-links-mobile").style.display = "flex";
+    }
+    function hideNavbar(){
+        document.querySelector(".header-nav-links-mobile").style.display = "none";
+    }
   return (
     <>
-      <div className="ClubEvent__page">
-        <div className="ClubEvent__container">
-          <div className="ClubEvent__heading">
-            <h1>Club Events</h1>
-          </div>
-
-          <div className="search_filter">
-            <form className="search">
-              <label>Search</label>
-              <input type="text" />
-            </form>
-          </div>
-
-          <div className="ClubEvent__section">
-            <div className="ClubEvents">
-              {
-                [0, 1, 2, 3, 4].map((i) => (
-
-                  <div className="ClubEvent__card">
-                    <h2 className="ClubEvent__name">Club Event {i + 1}</h2>
-                    <h3 className="ClubEvent_body">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                      Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                      laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                      irure dolor in reprehenderit in voluptate velit esse cillum
-                      dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                      cupidatat non proident, sunt in culpa qui officia deserunt
-                      mollit anim id est laborum.
-                    </h3>
-                    <div className="tag__area">
-                      <h3 className="ClubEvent__tag tag__header">tags: </h3>
-                      <div className="ClubEvent__tag">
-                        {
-                          [0, 1, 2, 3, 4].map((j) => (
-                            <div className="tag">tag{j + 1}</div>
-                          ))
-                        }
-                      </div>
-                    </div>
-                  </div>
-
-                ))
-              }
+        <header className='header'>
+            <div className='logo-container'>
+                <img src={require("../assets/logo.png")} alt="" />
+                <h2>PlaceXP</h2>
             </div>
-          </div>
+
+            <div className='header-nav-links'>
+                <a href=''>Home</a>
+                <a href=''>Interviews</a>
+                <a href=''>Hackathons</a>
+                <a href='' className='active'>Club Events</a>
+                <a href=''>Contact Us</a>
+                <img onClick={showNavbar} className='mobile-menu' src="https://img.icons8.com/ios-filled/50/000000/menu--v4.png"/>
+            </div>
+            
+
+        </header>
+        <div className='header-nav-links-mobile'>
+            <img onClick={hideNavbar} className='close-btn' src="https://img.icons8.com/material-rounded/24/000000/cancel--v2.gif"/>
+                <a href=''>Home</a>
+                <a href=''>Interviews</a>
+                <a href=''>Hackathons</a>
+                <a href='' className='active'>Club Events</a>
+                <a href=''>Contact Us</a>
         </div>
-      </div>
+
+        <div className='clubEvents'>
+            <div className='clubEvents-container'>
+                <div className='clubEvents-left'>
+                    <h2>ALL ABOUT CLUB EVENTS</h2>
+                    <h1>Get it here.</h1>
+                    <svg width="130" height="25" className='line-style' viewBox="0 0 183 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3 26C144.6 -0.220646 180 1.23606 180 5.24199" stroke="#8372EB" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>
+                <div className='search'>
+                    <input type="text" className='search-bar' placeholder='Search..'/>
+                    <input type="button" className='filter-btn' value='Filter' />
+                </div>
+            </div>
+
+            <div className='clubEvents-box'>
+            {
+                [0, 1, 2, 3, 4, 6].map((i) => (
+                    <div className='clubEvents-card'>
+                        <div className='clubEvents-sub-card'>
+                            <h1>EVENT {i+1}</h1>
+                            <p>Description: Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div className='Tags'>
+                        <h3>Tags:&nbsp;</h3>
+                        {
+                            [0, 1, 2].map((j) => (
+                            <h3>Tag{j}&nbsp;</h3>
+                            ))
+                        }
+                        </div>
+                    </div>
+                ))
+            }  
+            </div>
+            
+        </div>
+        
     </>
-  );
-};
+    );
+}
+
 export default ClubEvent;
